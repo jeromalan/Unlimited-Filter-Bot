2b91eac321b381d726013da97838712ffbba2473
jeromalan <73242169+jeromalan@users.noreply.github.com>
2021-05-12 18:15:43 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,11 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    InlineKeyboardButton("Group", url="https://t.me/DraXmovieZ"),
+                    InlineKeyboardButton("Channel", url="https://t.me/DraXMovies_Drax")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("About Me", callback_data="about_data"),   
                 ]
             ]
         )
@@ -70,7 +69,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/DraXMovies_Drax")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
